80ec53e728b886b5bfd1f495c4fd40742826b886
HadichaRaxmat <khadicha.raxmat@gmail.com>
2025-08-25 14:39:22 +0500

    Added Payement for competition

--- competition/models.py
+++ competition/models.py
@@ -37,6 +37,16 @@ class Application(BaseModel):
         return f"{self.parent} → {self.competition}"
 
 
+class CompetitionPayment(BaseModel):
+    payment_id = models.CharField(max_length=100, unique=True, verbose_name=_("payment ID"))
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("price"))
+    date_time = models.DateTimeField(default=timezone.now, verbose_name=_("date time"))
+
+    def __str__(self):
+        return f"Payment {self.payment_id} - {self.price}"
+
+
+
 
 
 class CompetitionSubscriber(BaseModel):
